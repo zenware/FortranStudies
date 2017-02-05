bbc26db5083bd1a5733db4f76c9328c5bbf1d419
Jay Looney <jay.m.looney@gmail.com>
2017-02-04 22:55:04 -0600

    Updated to use a TYPE(circle)

--- MiscExamples/circle_area.f90
+++ MiscExamples/circle_area.f90
@@ -3,14 +3,18 @@ PROGRAM circle_area
       ! reads a value representing the radius of a circle
       ! then calculates and writes out the area of the circle
 
-      REAL :: radius, area
+      TYPE circle
+          REAL :: radius, area!, pos(2)
+      END TYPE circle
+
       REAL, PARAMETER :: pi=3.141592653
+      TYPE(circle) :: my_circ
 
       WRITE (*,*) "This program calculates the area of a circle based on it's radius."
 
       WRITE (*,*) 'Please type the value for the radius:'
-      READ (5,*) radius
-      area = pi*radius*radius ! calculate the area
-      WRITE (*,*) 'The area of a circle of radius ', radius, ' is ', area
+      READ (5,*) my_circ%radius
+      my_circ%area = pi*my_circ%radius**2! calculate the area
+      WRITE (*,*) 'The area of a circle of radius ', my_circ%radius, ' is ', my_circ%area
 
 END PROGRAM circle_area
